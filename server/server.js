99c4f95945eb55c8d237a3a2cc0d5f06f3dfff63
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-11 16:15:12 +0000

    Start the server
    app.listen() takes in the port 8000and a callback function that logs 'Hello from server!' to the console

--- server/server.js
+++ server/server.js
@@ -13,3 +13,8 @@ app.use(express.json());
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+// Start the server
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000.`);
+});
