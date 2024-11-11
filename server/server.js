9ecffe5633fb28ee2f45f714121f6e178e8f6ff6
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-11 16:05:48 +0000

    Create the Express application
    stored express() in the app variable
    Using cors to allow cross-origin requests
    app.use() to add the cors middleware to the Express application
    using express.json() to parse the incoming requests with JSON payloads

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
+// Import express and cors
 const express = require("express");
 const cors = require("cors");
+
+// Create the Express application
+const app = express();
+
+app.use(cors());
+app.use(express.json());
