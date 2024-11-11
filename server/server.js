a3cd67421ef9c7a92ebd23ddd6f2274b99f3d506
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-11 16:36:49 +0000

    Revert "Start the server"
    This reverts commit 99c4f95945eb55c8d237a3a2cc0d5f06f3dfff63.

--- server/server.js
+++ server/server.js
@@ -13,8 +13,3 @@ app.use(express.json());
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-
-// Start the server
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
-});
