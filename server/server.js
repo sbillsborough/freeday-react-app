93bf5877bee19ede385e40ed74b0779ef937f7d1
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-11 16:45:34 +0000

    Start the server using app.listen()
    app.listen() takes two arguments, the port number on which the server is running (8000) and a callback that's called when the server starts which logs the message that the server is running

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 // Create a GET route
-
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+//Start the server
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000.`);
 });
