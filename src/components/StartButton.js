25b7480d2f063ec76dd4ffad4f8a223a230c9eb7
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-15 17:17:54 +0000

    Button text updated to Add User instead of Contained

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "@mui/material";
 
 function StartButton() {
-  return <Button variant="contained">Contained</Button>;
+  return <Button variant="contained">Add User</Button>;
 }
 
 export default StartButton;
