20a5165d355b0f41188906d2667887353985e02e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-22 14:56:46 +0000

    AddUserButton
    Added an Events.jsx which contains the querySelector and eventListener for the AddUser button
    Exported the addUserButton variable containing the selected button element
    Added the eventListener to the button, listen for click and console.log confirmation of click
    Imported the addUserButton to the StartButton component

---
+++ src/components/Events.jsx
@@ -0,0 +1,8 @@
+import StartButton from "./StartButton.jsx";
+
+// Query Selectors and Event Listeners
+
+export const addUserButton = document.querySelector(".MuiButton-root");
+addUserButton.addEventListener("click", () => {
+  console.log("Hello I've been clicked!");
+});
--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "@mui/material";
+import { addUserButton } from "./Events.jsx";
 
 function StartButton() {
   return <Button variant="contained">Add User</Button>;
