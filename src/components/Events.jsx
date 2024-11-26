12795cc0675e3f94025861e8aa3372634406835e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-26 17:33:32 +0000

    Fixed after breaking the server
    Re-added necessary server.js that was accidentaly deleted
    removed src from root directory as there's one in the correct client directory

--- src/components/Events.jsx
+++
@@ -1,9 +0,0 @@
-// import StartButton from "./StartButton.jsx";
-
-// // Query Selectors and Event Listeners
-
-// export const addUserButton = document.querySelector(".MuiButton-root");
-
-// addUserButton.addEventListener("click", () => {
-//   console.log("Hello I've been clicked!");
-// });
