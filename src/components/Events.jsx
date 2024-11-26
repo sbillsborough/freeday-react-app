6403bf000571d3664edb3ed9e61a22fc9bec4cf2
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-26 17:42:03 +0000

    Working again
    src folder correctly placed in the client directory

---
+++ src/components/Events.jsx
@@ -0,0 +1,9 @@
+// import StartButton from "./StartButton.jsx";
+
+// // Query Selectors and Event Listeners
+
+// export const addUserButton = document.querySelector(".MuiButton-root");
+
+// addUserButton.addEventListener("click", () => {
+//   console.log("Hello I've been clicked!");
+// });
