0f07f0281221cbd18b39dbcba2794562837e90dc
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-11-27 15:47:30 +0000

    Remove StartButton.jsx
    no longer needed, using AddUserButton.jsx instead

--- src/components/StartButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "@mui/material";
-
-function StartButton() {
-  return <Button variant="contained">Add User</Button>;
-}
-
-export default StartButton;
