8c65e12340780b287c7f813f91f01d863db64795
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-12-03 16:45:10 +0000

    Removed the deprecated options from db.js
    removed useNewUrlParser and useInifiedTopology from mongoose.connect()

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("Database connection error:", err.message);
     process.exit(1);
