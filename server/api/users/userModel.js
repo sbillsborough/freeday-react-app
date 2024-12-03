3268700a4f46d384fe66785605b9de54b3eaa88e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-12-03 16:32:54 +0000

    fixed the Schema spelling mistake and added .env to gitignore

--- server/api/users/userModel.js
+++ server/api/users/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.scheema({
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
