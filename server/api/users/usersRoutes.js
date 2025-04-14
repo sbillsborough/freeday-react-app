08eef3b76c12ac46a8fd7021ade17f46ed783847
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-04-14 13:18:41 +0100

    Check if a user already has an ID
    Checks if a user has an ID
    If true switch to a PUT method to update the user rather than create a new user
    Updated userRoutes to handle a PUT request
    Added updateUser to usersController.js

--- server/api/users/usersRoutes.js
+++ server/api/users/usersRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/users", createUser); // Save user
 router.get("/users", getUsers); // Retrieve users
+router.put("/users/:id", updateUser); // Update user by ID
 
 export default router;
